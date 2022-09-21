8694c3598dcbcd49127a2800c6440da50608effb
Шкурат Данил Евгеньевич <shkurat.d@spbec-mining.ru>
2022-09-21 13:18:31 +0300

    Initial commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace AvaloniaCalc {
+    public partial class MainWindow:Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
