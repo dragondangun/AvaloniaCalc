c5e9d4896967753b66323317718f31a831965e29
Шкурат Данил Евгеньевич <shkurat.d@spbec-mining.ru>
2022-09-21 15:22:39 +0300

    Handle numeric buttons click

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
 
 namespace AvaloniaCalc {
     public partial class MainWindow:Window {
@@ -6,5 +8,25 @@ namespace AvaloniaCalc {
             InitializeComponent();
             historyLabel.Content = "";
         }
+
+        private void numberButton_OnClick(object? sender, RoutedEventArgs args) {
+            string? currentString = (currentLabel.Content as string);
+            bool? isFractional = currentString?.Contains(',');
+
+            //explicit bool comprasion cause of nullable bool
+            if(isFractional==false && currentString?.Length >= 16 || isFractional==true && currentString?.Length >= 17) {
+                return;
+            }
+
+            if(currentString?.Length == 1 && Convert.ToDouble(currentString) == 0) {
+                currentLabel.Content = (sender as Button)?.Content;
+                return;
+            }
+
+            string? senderString = ((sender as Button)?.Content as string);
+
+            currentLabel.Content = $"{currentString}{senderString}";
+        }
+
     }
 }
